1ded1eadcbc18f3dd0d7ba004a71ee3622942a52
WMoth <dldngud97@naver.com>
2022-04-12 17:38:08 +0900

    Project Start

---
+++ Project_DB/Assets/Scripts/GameManager.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager instance;
+
+    public static GameManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+
+        var objs = FindObjectsOfType<GameManager>();
+        if(objs.Length != 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+
+        instance = this;
+    }
+
+    private void Update()
+    {
+        Debug.Log("Hello");
+    }
+}
